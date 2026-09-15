25341d2516fbb1c0a344500b86ea453d3716e102
jpl <jpl@noydb.com>
2026-09-15 18:25:22 -0400

    build out nfcc2

--- azure/providers.tf
+++ azure/providers.tf
@@ -1,8 +1,13 @@
 provider "azurerm" {
   features {}
 
-  resource_provider_registrations = "all"
-  storage_use_azuread             = true
+  resource_provider_registrations = "none"
+  resource_providers_to_register  = [
+    "Microsoft.Batch",
+    "Microsoft.Storage",
+    "Microsoft.Authorization",
+  ]
+  storage_use_azuread = true
 }
 
 provider "azuread" {}
